44cf6a31d82dd515b3aa2c7e3b338e02a2cb4fee
Gabriela Azar <gabriela.azar@ort.edu.ar>
2022-09-25 16:20:29 -0300

    Subo toda la página

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+function myFunction1() {
+  let valor = document.querySelector("#estacion1").value;
+ if (valor === "221424") {
+    window.location=("/estacion2.html")
+     
+ } else {
+    alert("CÓDIGO INCORRECTO")  
+ }
+}
+
+
+
+
+function myFunction2() {
+   let valor = document.querySelector("#estacion2").value;
+  if (valor === "174") {
+     window.location=("/estacion3.html")
+      
+  } else {
+     alert("CÓDIGO INCORRECTO")  
+  }
+ }
+
+ 
+function myFunction3() {
+   let valor = document.querySelector("#estacion3").value;
+  if (valor === "351") {
+     window.location=("/estacion4.html")
+      
+  } else {
+     alert("CÓDIGO INCORRECTO")  
+  }
+ }
+
+ function myFunction4() {
+   let valor = document.querySelector("#estacion4").value;
+  if (valor === "24531") {
+     window.location=("/estacion5.html")
+      
+  } else {
+     alert("CÓDIGO INCORRECTO")  
+  }
+ }
+ function myFunction5() {
+   let valor = document.querySelector("#estacion5").value;
+  if (valor === "31987") {
+     window.location=("/final.html")
+      
+  } else {
+     alert("CÓDIGO INCORRECTO")  
+  }
+ }
+
+
+
